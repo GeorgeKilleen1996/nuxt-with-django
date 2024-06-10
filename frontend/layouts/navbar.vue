77eff0d85a187c8f82f365d7b77b0ca0c632b1c2
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2024-06-10 23:21:19 +0100

    feat(frontend): add dynamic navbar based on logged in state

--- frontend/layouts/navbar.vue
+++ frontend/layouts/navbar.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const token = useCookie('is_logged_in_token');
+</script>
 
 <template>
     <div class="flex-1 h-14 border-b">
@@ -6,9 +8,12 @@
             <div class="flex items-center gap-4">
                 <NuxtLink to="/" class="text-xl font-bold">NuxtJS</NuxtLink>
             </div>
-            <div class="flex items-center gap-4">
+            <div class="flex items-center gap-4" v-if="!token">
                 <NuxtLink to="/auth/login" class="text-sm">Login</NuxtLink>
                 <NuxtLink to="/auth/register" class="text-sm">Register</NuxtLink>
+            </div>
+            <div class="flex items-center gap-4" v-else>
+                <div class="w-9 h-9 rounded"></div>
             </div>
         </div>
     </div>
