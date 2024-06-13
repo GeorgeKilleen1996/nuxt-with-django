681ac76647177326976ed78a2b500df2f1d9c7ff
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2024-06-13 22:43:47 +0100

    feat(frontend): create UI Popover component

--- frontend/layouts/navbar.vue
+++ frontend/layouts/navbar.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+
 const token = useCookie('is_logged_in_token');
+
+const userMenuOpen = ref(false);
 </script>
 
 <template>
@@ -13,10 +16,29 @@ const token = useCookie('is_logged_in_token');
                 <NuxtLink to="/auth/register" class="text-sm">Register</NuxtLink>
             </div>
             <div class="flex items-center gap-4" v-else>
-                <button type="button" name="UserMenuButton" class="w-9 h-9 rounded flex justify-center items-center hover:text-primary hover:bg-primary-10 text-secondary-light">
-                    <Icon name="ion:person" />
-                </button>
+                <div class="relative">
+                    <button type="button" name="UserMenuButton" @click="userMenuOpen = !userMenuOpen" class="w-9 h-9 rounded flex justify-center items-center hover:text-primary hover:bg-primary-10 text-secondary-light">
+                        <Icon name="ion:person" />
+                    </button>
+                    <Transition name="fade-down" mode="out-in">
+                        <UIPopover class="absolute -bottom-10 right-0 max-w-40" v-if="userMenuOpen"></UIPopover>
+                    </Transition>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.fade-down-enter-active, .fade-down-leave-active {
+    transition: opacity 0.3s, transform 0.3s;
+}
+.fade-down-enter-from, .fade-down-leave-to {
+    opacity: 0;
+    transform: translateY(-10px);
+}
+.fade-down-enter-to, .fade-down-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+}
+</style>
