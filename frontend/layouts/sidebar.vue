9e9a75ed6d755eb741c06ec6f6f0a61b827772f2
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2024-06-13 21:37:25 +0100

    style(frontend): add sidenav opening transition

--- frontend/layouts/sidebar.vue
+++ frontend/layouts/sidebar.vue
@@ -22,7 +22,10 @@ const hasNext = (index: number) => index < props.navItems.length - 1;
 </script>
 
 <template>
-    <div class="h-screen w-14 border-r border-tertiary fixed top-0 left-0 hover:shadow-sm hover:shadow-tertiary z-50 hover:w-fit transition-all overflow-hidden bg-secondary" @mouseover="navOpen = true" @mouseleave="navOpen = false">
+    <div 
+        class="navbar-transition h-screen border-r border-tertiary fixed top-0 left-0 hover:shadow-sm hover:shadow-tertiary z-50 transition-all bg-secondary" 
+        :class="{'w-14' : !navOpen, 'w-56' : navOpen}"
+        @mouseover="navOpen = true" @mouseleave="navOpen = false">
         <div class="flex flex-col h-full">
             <div class="h-14 w-full p-2.5 flex justify-start items-center"></div>
             <div class="flex-1 flex flex-col p-2 gap-2">
@@ -31,10 +34,12 @@ const hasNext = (index: number) => index < props.navItems.length - 1;
                         v-for="item in items.items"
                         :to="item.to"
                         :exact="item.exact"
-                        class="flex gap-2 h-9 rounded hover:bg-primary-10 p-2.5 overflow-hidden"
+                        class="flex gap-2 rounded hover:bg-primary-10 p-2.5 items-center"
                         :class="{'bg-primary-10 text-primary hover:text-primary-light' : item.active, 'hover:text-primary text-secondary-light': !item.active , 'w-full' : navOpen, 'w-9': !navOpen}"
                     >
-                        <Icon :name="item.icon" />
+                        <div class="flex-shrink-0 flex items-center">
+                            <Icon :name="item.icon" />
+                        </div>
                         <span class="text-xs max-w-40 overflow-hidden truncate" v-if="navOpen">{{ item.title }}</span>
                     </NuxtLink>
                     <div class="w-full h-0 border-b border-tertiary" v-if="hasNext(index)"></div>
@@ -43,3 +48,9 @@ const hasNext = (index: number) => index < props.navItems.length - 1;
         </div>
     </div>
 </template>
+
+<style scoped>
+.navbar-transition {
+  transition: width 0.3s ease;
+}
+</style>
